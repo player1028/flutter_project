16681d9ee5616588ec040cfa423eeb1fac30a011
Omarbek Nurkut <omarbeknurkut6@gmail.com>
2023-06-16 21:17:59 +0600

    Hello

---
+++ lib/screens/add_page.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController describeController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add todo'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            decoration: InputDecoration(hintText: 'Title'),
+            controller: titleController,
+          ),
+          TextField(
+            decoration: InputDecoration(hintText: 'Describe'),
+            keyboardType: TextInputType.multiline,
+            controller: describeController,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(onPressed: (){
+            postData();
+          }, child: Text('Submit'))
+        ],
+      ),
+    );
+  }
+
+
+
+  Future<void> postData() async {
+    final title = titleController.text;
+    final description = describeController.text;
+    final body = {
+      'title': title,
+      'description': description,
+      'is_completed': false,
+    };
+
+    final url = 'http://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+        uri,
+        body: jsonEncode(body),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    );
+
+
+    void showSuccessMessage(String message) {
+      titleController.text = '';
+      describeController.text = '';
+      final snackBar = SnackBar(content: Text(message));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+
+    void showErrorMessage(String message) {
+      final snackBar = SnackBar(content: Text(message,
+        style: TextStyle(color: Colors.white),
+      ), backgroundColor: Colors.red,);
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+
+
+    if(response.statusCode == 201) {
+      showSuccessMessage('Success');
+    } else {
+      showErrorMessage('Error');
+    }
+
+
+  }
+}
---
+++ lib/screens/todolist.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:untitled/screens/add_page.dart';
+import 'package:http/http.dart' as http;
+
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+
+  List items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          var item = items[index];
+          return ListTile(
+            title: Text(item['title']),
+            subtitle: Text(item['description']),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          navigateAddPage(context);
+        },
+        label: Text('Add todo'),
+      ),
+    );
+  }
+
+  void navigateAddPage(context) {
+    final route = MaterialPageRoute(builder: (context) => AddTodoPage());
+    Navigator.push(context, route);
+  }
+
+  Future<void> fetchData() async {
+    final url = 'http://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if(response.statusCode == 200) {
+      var json = jsonDecode(response.body) as Map;
+      var result = json['items'];
+      setState(() {
+        items = result;
+      });
+    }
+  }
+}
